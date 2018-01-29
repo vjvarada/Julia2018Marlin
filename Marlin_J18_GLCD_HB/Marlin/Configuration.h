cfb0518c220a94cf4550d2453508a5f04b1cefb2
Vijay Raghav Varada <vjvarada@fracktal.in>
2018-01-29 11:16:57 +0530

    changed jerk settings

--- Marlin_J18_GLCD_HB/Marlin/Configuration.h
+++ Marlin_J18_GLCD_HB/Marlin/Configuration.h
@@ -546,8 +546,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 20.0
+#define DEFAULT_XJERK                  10.0
+#define DEFAULT_YJERK                  5.0
 #define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
