e837df68cd4453497bcdfe0bc41d6d8154a8558b
Vijay Raghav Varada <vjvarada@fracktal.in>
2018-02-23 14:10:03 +0530

    changed temp settings to prevent false positives

--- Marlin_J18_GLCD/Marlin/Configuration.h
+++ Marlin_J18_GLCD/Marlin/Configuration.h
@@ -284,7 +284,7 @@
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
 // Extruder temperature must be close to target for this long before M109 returns success
-#define TEMP_RESIDENCY_TIME 2  // (seconds)
+#define TEMP_RESIDENCY_TIME 1  // (seconds)
 #define TEMP_HYSTERESIS 4       // (degC) range of +/- temperatures considered "close" to the target one
 #define TEMP_WINDOW     1       // (degC) Window around target to start the residency timer x degC early.
 
--- Marlin_J18_GLCD_HB/Marlin/Configuration_adv.h
+++ Marlin_J18_GLCD_HB/Marlin/Configuration_adv.h
@@ -63,7 +63,7 @@
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
   #define THERMAL_PROTECTION_PERIOD 80        // Seconds
-  #define THERMAL_PROTECTION_HYSTERESIS 10     // Degrees Celsius
+  #define THERMAL_PROTECTION_HYSTERESIS 15     // Degrees Celsius
 
   /**
    * Whenever an M104 or M109 increases the target temperature the firmware will wait for the
